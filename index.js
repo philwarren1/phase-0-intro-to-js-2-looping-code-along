691a3a3b744b8a0e823d2d1d9286c29122f94473
Philip <philwarren1@gmail.com>
2021-04-11 15:53:23 -0400

    commit tree a8a1f15bea5219b4979d058a34561401a5ff35df

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const names = []
+
+function writeCards(names, event) {
+    let newCards = []
+    for (let i = 0; i < names.length; i++) { 
+        newCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newCards
+}
+
+writeCards(names, "birthday")
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number -= 1;
+}
+
+};
+
+
+countDown(9);
+//       1) returns an array of thank you messages for each name provided to the function
+//   countDown()
+//       2) invokes console.log once for each number, counting down from the number provided to zero
+//       3) logs each number when counting down, starting from the number provided
